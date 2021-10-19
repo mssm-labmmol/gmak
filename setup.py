b3af05a6e4445e1a341150eb618723f45aa494bb
Yan <yanmarques@gmail.com>
2021-10-18 23:14:37 -0300

    post-processing + pandas pareto

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import sys
+
+try:
+    import pandas_pareto.pareto
+except:
+    sys.exit("First install pandas_pareto:\n\ncd pandas_pareto\npython setup.py"
+             " install\n")
 
 setup (
     name='gmak',
