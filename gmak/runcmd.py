d74b72b0e7027b79371dddd8b5a2f36c5c7e1bb3
Yan <yanmarques@gmail.com>
2021-10-30 09:40:32 -0300

    completed configuration core

--- gmak/runcmd.py
+++ gmak/runcmd.py
@@ -1,5 +1,7 @@
 import subprocess
 import os
+from gmak.config import ConfigVariables
+import re
 
 no_backup_environ = dict(os.environ, GMX_MAXBACKUP="-1")
 
@@ -17,3 +19,28 @@ def run(cmd):
                              env=no_backup_environ)
         # Communicate stdin.
         p.communicate(input=command2input)
+
+
+def gmx_insert_molecules(args_list):
+    default_try = '1000'
+    from gmak.configurations import ConfigurationError
+    # number of molecules requested
+    nmol = int(args_list[args_list.index("-nmol") + 1])
+    # set try if not given in args_list
+    if '-try' not in args_list:
+        args_list += ['-try', default_try]
+    # save stdout to check if requested number of molecules was
+    # reached
+    log = subprocess.check_output(
+        [ConfigVariables.gmx, "insert-molecules"] + args_list,
+        env=no_backup_environ,
+        stderr=subprocess.STDOUT).decode('utf-8')
+    # check number of added molecules
+    m = re.search('Added ([0-9])+ molecules', log)
+    nmol_added = int(m.group(1))
+    if nmol != nmol_added:
+        raise ConfigurationError(f"Requested {nmol} molecules but "
+                                 "gmx insert-molecules was able "
+                                 f"to insert only {nmol_added}.")
+    
+    
